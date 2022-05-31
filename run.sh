fdaaf43e9ec0c19a42d9eb9ee41f85a33a5e699f
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2022-05-30 23:08:39 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 mvn clean package -Dmaven.test.skip=true 
 
-java  -jar  --enable-preview --add-modules jdk.incubator.concurrent -Xdebug -Dconfig.env=local -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Djdk.tracePinnedThreads=full ./target/loomy-0.0.1-SNAPSHOT.jar
+java  -jar -Dloom=true --enable-preview --add-modules jdk.incubator.concurrent -Xdebug -Dconfig.env=local -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Djdk.tracePinnedThreads=full ./target/loomy-0.0.1-SNAPSHOT.jar
