6ea8ace8f609533e84611d640232c00cdf1b91d9
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-11-25 20:29:58 -0500

    SB 3.2

--- src/main/java/com/loom/loomy/LoomyApplication.java
+++ src/main/java/com/loom/loomy/LoomyApplication.java
@@ -2,14 +2,8 @@ package com.loom.loomy;
 
 import java.time.temporal.ChronoUnit;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.embedded.jetty.ConfigurableJettyWebServerFactory;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.context.annotation.Bean;
-
-import com.loom.loomy.config.LoomThreadPool;
 
 @SpringBootApplication
 public class LoomyApplication {
@@ -19,17 +13,5 @@ public class LoomyApplication {
   public static void main(String[] args) {
 
     new SpringApplicationBuilder(LoomyApplication.class).run(args);
-  }
-
-  @Bean
-  public WebServerFactoryCustomizer<ConfigurableJettyWebServerFactory> jettyPoolCustomizer(
-      @Value("${loom:true}") boolean loom) {
-
-    return loom
-        ? server -> {
-          System.out.println("Using Loom");
-          server.setThreadPool(new LoomThreadPool());
-        }
-        : server -> System.out.println("Not Using Loom");
   }
 }
